8488d53f20686d4c89b848e3ae9d1806c1a00875
broin <779656172@qq.com>
2024-05-26 00:55:58 +0800

    not finished phase yet...

--- ebpf/container/.output/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/root/bpf-test/hela/ebpf/container/.output/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /root/bpf-test/hela/bpftool/include/linux/compiler.h \
- /root/bpf-test/hela/bpftool/include/linux/compiler_types.h \
- /root/bpf-test/hela/bpftool/include/linux/compiler-gcc.h
